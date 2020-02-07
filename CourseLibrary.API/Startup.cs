7481ea7ccfb284ba3aa29cc159c56c0d576abcd8
Raul Magana <bucanero14@gmail.com>
2020-02-06 19:43:14 -0600

    Added PUT and PATCH support

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -4,11 +4,13 @@ using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
 using System;
 
 namespace CourseLibrary.API
@@ -28,7 +30,32 @@ namespace CourseLibrary.API
            services.AddControllers(setupAction =>
            {
                setupAction.ReturnHttpNotAcceptable = true;
-           }).AddXmlDataContractSerializerFormatters();
+           }).AddNewtonsoftJson(setupAction =>
+           {
+               setupAction.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+           })
+           .AddXmlDataContractSerializerFormatters()
+           .ConfigureApiBehaviorOptions(setupAction =>
+           {
+               setupAction.InvalidModelStateResponseFactory = context =>
+               {
+                   var problemDetails = new ValidationProblemDetails(context.ModelState)
+                   {
+                       Type = "https://courselibrary.com/modelvalidationproblem",
+                       Title = "One or mode model validation errors occured.", 
+                       Status = StatusCodes.Status422UnprocessableEntity,
+                       Detail = "See the errors property for details",
+                       Instance = context.HttpContext.Request.Path
+                   };
+
+                   problemDetails.Extensions.Add("traceId", context.HttpContext.TraceIdentifier);
+
+                   return new UnprocessableEntityObjectResult(problemDetails)
+                   {
+                       ContentTypes = { "application/problem+json" }
+                   };
+               };
+           });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
              
